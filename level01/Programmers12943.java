923cc572d02b3a8d2435fdeda75f55b31af43efb
Pangtaek <zombie3632@gmail.com>
2024-09-07 23:27:23 +0900

    콜라추측

---
+++ level01/Programmers12943.java
@@ -0,0 +1,22 @@
+package level01;
+
+public class Programmers12943 {
+    public int solution(int num) {
+        int answer = 0;
+
+        while(num != 1) {
+            if(num % 2 == 0)
+                num /= 2;
+            else
+                num = num * 3 + 1;
+            answer++;
+
+            if(answer >= 500) {
+                answer = -1;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
