b3f7b880642f63dbab1c34ea44683f5c280b614a
Pangtaek <zombie3632@gmail.com>
2025-01-29 19:59:02 +0900

    refactor: 우선순위 큐를 사용하여 코드 리펙토링

--- level02/Programmers172927.java
+++ level02/Programmers172927.java
@@ -15,7 +15,8 @@ public class Programmers172927 {
 
     public int solution(int[] picks, String[] minerals) {
         int answer = 0;
-        List<int[]> mineralGroups = new ArrayList<>();
+        PriorityQueue<MineralSet> mineralQueue = new PriorityQueue<>(
+                (m1, m2) -> Integer.compare(m2.getTotalFatigue(), m1.getTotalFatigue()));
 
         int totalPicks = Arrays.stream(picks).sum();
         int maxMiningAmount = Math.min(totalPicks * 5, minerals.length);
@@ -30,37 +31,53 @@ public class Programmers172927 {
             }
 
             if ((i + 1) % 5 == 0 || i == maxMiningAmount - 1) {
-                mineralGroups.add(new int[]{diaCount, ironCount, stoneCount});
+                mineralQueue.add(new MineralSet(diaCount, ironCount, stoneCount));
                 diaCount = 0;
                 ironCount = 0;
                 stoneCount = 0;
             }
         }
 
-        mineralGroups.sort((a, b) -> {
-            if (b[0] != a[0]) return Integer.compare(b[0], a[0]);
-            if (b[1] != a[1]) return Integer.compare(b[1], a[1]);
-            return Integer.compare(b[2], a[2]);
-        });
-
         int pickIndex = 0;
-        for (int[] group : mineralGroups) {
+        while (!mineralQueue.isEmpty()) {
             while (pickIndex < 3 && picks[pickIndex] == 0) {
                 pickIndex++;
             }
             if (pickIndex == 3) break;
 
-            int dia = group[0], iron = group[1], stone = group[2];
-            if (pickIndex == 0) {
-                answer += dia + iron + stone;
-            } else if (pickIndex == 1) {
-                answer += dia * 5 + iron + stone;
-            } else {
-                answer += dia * 25 + iron * 5 + stone;
-            }
-
+            MineralSet mineralSet = mineralQueue.poll();
+            answer += mineralSet.getFatigue(pickIndex);
             picks[pickIndex]--;
         }
         return answer;
+    }
+}
+
+class MineralSet {
+    private static final int DIA_FATIGUE = 25;
+    private static final int IRON_FATIGUE = 5;
+    private static final int STONE_FATIGUE = 1;
+
+    private int diaCount;
+    private int ironCount;
+    private int stoneCount;
+
+    public MineralSet(int diaCount, int ironCount, int stoneCount) {
+        this.diaCount = diaCount;
+        this.ironCount = ironCount;
+        this.stoneCount = stoneCount;
+    }
+
+    public int getTotalFatigue() {
+        return diaCount * DIA_FATIGUE + ironCount * IRON_FATIGUE + stoneCount * STONE_FATIGUE;
+    }
+
+    public int getFatigue(int pickIndex) {
+        return switch (pickIndex) {
+            case 0 -> diaCount + ironCount + stoneCount;
+            case 1 -> diaCount * 5 + ironCount + stoneCount;
+            case 2 -> diaCount * 25 + ironCount * 5 + stoneCount;
+            default -> 0;
+        };
     }
 }
