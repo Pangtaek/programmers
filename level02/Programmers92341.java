6a75d35a2930a99b531e4f76d591cf37673b8362
Pangtaek <zombie3632@gmail.com>
2025-03-15 22:13:47 +0900

    주차 요금 계산

--- level02/Programmers92341.java
+++ level02/Programmers92341.java
@@ -2,10 +2,14 @@ package level02;
 
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 /*
  * 문제: 주차 요금 계산
@@ -16,8 +20,8 @@ import java.util.TreeMap;
 public class Programmers92341 {
 
     public static void main(String[] args) {
-        int[] fees = {180, 5000, 10, 600};  // 기본 시간(분), 기본 요금(원), 단위 시간(분), 단위 요금(원)
-        String[] records = {                // 입출력 시간, 차량 번호, 입출력 정보
+        int[] fees = { 180, 5000, 10, 600 }; // 기본 시간(분), 기본 요금(원), 단위 시간(분), 단위 요금(원)
+        String[] records = {
                 "05:34 5961 IN",
                 "06:00 0000 IN",
                 "06:34 0000 OUT",
@@ -33,48 +37,48 @@ public class Programmers92341 {
     }
 
     public static int[] solution(int[] fees, String[] records) {
-        HashMap<String, LocalTime> inTimeMap = new HashMap<>();  // 차량 번호, 입차 시간
-        TreeMap<String, Integer> totalParkingTime = new TreeMap<>(); // 차량 번호, 총 주차 시간
+        Map<Integer, LocalTime> timeMap = new HashMap<>();
+        Map<Integer, Integer> chargeMap = new TreeMap<>(); // 차량번호를 오름차순으로 정렬
+        Set<Integer> set = new TreeSet<>(); // 주차장에 남아있는 차량을 저장
 
         for (String record : records) {
-            String[] tokens = record.split(" ");
-            LocalTime time = LocalTime.parse(tokens[0]);
+            String[] data = record.split("\\s+");
+            LocalTime time = LocalTime.parse(data[0]);
+            int carNumber = Integer.parseInt(data[1]);
+            String io = data[2];
 
-            // 차량이 주차장에 입차하는 경우
-            if (tokens[2].equals("IN")) {
-                inTimeMap.put(tokens[1], time);
-            } else { // 차량이 주차장에서 출차하는 경우
-                LocalTime inTime = inTimeMap.remove(tokens[1]);
-                Duration duration = Duration.between(inTime, time);
-                totalParkingTime.put(tokens[1], totalParkingTime.getOrDefault(tokens[1], 0) + (int) duration.toMinutes());
+            if (io.equals("IN")) {
+                timeMap.put(carNumber, time);
+                set.add(carNumber);
+            } else { // OUT
+                LocalTime startInclusive = timeMap.get(carNumber);
+                Duration duration = Duration.between(startInclusive, time);
+
+                chargeMap.put(carNumber, chargeMap.getOrDefault(carNumber, 0) + (int) duration.toMinutes());
+                timeMap.remove(carNumber); // 출차했으므로 시간 기록 삭제
+                set.remove(carNumber);
             }
         }
 
-        // 출차하지 않은 차량 처리 (23:59에 출차한 것으로 간주)
-        LocalTime endOfDay = LocalTime.of(23, 59);
-        for (Map.Entry<String, LocalTime> entry : inTimeMap.entrySet()) {
-            String carNumber = entry.getKey();
-            LocalTime inTime = entry.getValue();
-            Duration duration = Duration.between(inTime, endOfDay);
-            totalParkingTime.put(carNumber, totalParkingTime.getOrDefault(carNumber, 0) + (int) duration.toMinutes());
+        // 출차 기록이 없는 차량을 23:59에 출차 처리
+        for (int carNumber : set) {
+            LocalTime startInclusive = timeMap.get(carNumber);
+            Duration duration = Duration.between(startInclusive, LocalTime.of(23, 59));
+
+            chargeMap.put(carNumber, chargeMap.getOrDefault(carNumber, 0) + (int) duration.toMinutes());
         }
 
-        // 요금 계산
-        int[] answer = new int[totalParkingTime.size()];
-        int index = 0;
+        List<Integer> answer = new ArrayList<>();
 
-        for (String carNumber : totalParkingTime.keySet()) {
-            int totalMinutes = totalParkingTime.get(carNumber);
-            int fee = fees[1]; // 기본 요금
-
-            if (totalMinutes > fees[0]) { // 기본 시간 초과
-                int extraMinutes = totalMinutes - fees[0];
-                fee += (int) (Math.ceil((double) extraMinutes / fees[2]) * fees[3]);
+        for (int duration : chargeMap.values()) {
+            if (duration <= fees[0]) {
+                answer.add(fees[1]); // 기본 시간 이내는 기본 요금
+            } else {
+                int extraFee = (int) Math.ceil((double) (duration - fees[0]) / fees[2]) * fees[3];
+                answer.add(fees[1] + extraFee);
             }
-
-            answer[index++] = fee;
         }
 
-        return answer;
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
